05f500f078d26725d049d677617c34401b5a91de
marceljones <marceljones.mj@gmail.com>
2020-09-10 16:29:22 -0400

    parks dropdown functioning TN only

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -8,8 +8,8 @@ export const useParks = () => {
 }
 
 export const getParks = () => {
-    return fetch("https://developer.nps.gov/api/v1/parks?stateCode=TN&stateCode=&api_key=F46doarbJ2YIbax1oEVtxx6YEx05v6l8XYFauMex")
-    .then(response => response.json())
+    return fetch(`https://developer.nps.gov/api/v1/parks?stateCode=TN&stateCode=&api_key=${Settings.npsKey}`)
+    .then(response => response.json()) 
     .then(parsedResponse => {
         console.log("test", parsedResponse)
         nationalParks = parsedResponse.data
