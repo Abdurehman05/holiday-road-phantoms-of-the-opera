6af169b3eb6519de925f7f605cf00041b06b188a
Goody <mattgoodman3099@gmail.com>
2020-09-10 18:44:53 -0500

    fixes to merge conflicts

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 import { EaterySelect } from "./eateries/EaterySelect.js";
 import { AttractionSelect } from "./attractions/AttractionSelect.js";
 
 EaterySelect();
 AttractionSelect();
-=======
+
 
 
 import { ParkSelect } from "./parks/ParkSelected.js"
@@ -20,13 +20,12 @@ import { getWeather } from './weather/WeatherProvider.js';
 
 EaterySelect();
 
-<<<<<<< HEAD
+
 
 weatherList();
-=======
+
 import { testing } from './itinerarypreview/ItineraryPreviewEatery.js'
 
 testing();
 
->>>>>>> 6cf1fb8c8427b79f517ae09c6aa041548e4c2da2
->>>>>>> master
+
