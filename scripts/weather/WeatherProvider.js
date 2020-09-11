26efe63599a8828230871cfd4119dab9734cf9a9
Goody <mattgoodman3099@gmail.com>
2020-09-11 12:56:19 -0500

    cleaning up the weatherAPI

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,4 +1,8 @@
+import { getParks, useParks } from "../parks/ParkProvider.js"
 import Settings from "../Settings.js"
+
+
+
 
 let weather = []
 
@@ -7,7 +11,13 @@ export const useWeather = () => {
 }
 
 export const getWeather = () => {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${Settings.weatherKey}`)
+    console.log("getWeather works")
+    let parks = useParks()
+    const targetPark = parks.find(park => {
+        return park.name === document.querySelector("#parkSelect").value
+    })
+    console.log("targetpark", targetPark)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${targetPark.addresses[0].postalCode}&appid=${Settings.weatherKey}`)
     .then(response => response.json())
     .then(parsedResponse => {
         weather = parsedResponse.list
--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -2,23 +2,25 @@
 
 export const weatherHTML = (weatherObj) => {
     //console.log("weatherObject test", weatherObj)
-    let d = new Date(weatherObj.dt)
-    let day = d.getDay()
-    let options = {month: 'long',}
-    let month = new Intl.DateTimeFormat('en-US', options).format(d)
-    let year = d.getFullYear()
-    //console.log(d)
+    let d = weatherObj.dt_txt
+    //console.log("date", d.toLocaleString('en-US'))
+    // let day = d.getDay()
+    // let options = {month: 'long',}
+    // let month = new Intl.DateTimeFormat('en-US', options).format(d)
+    // let year = d.getFullYear()
+    // //console.log(d)
     // let n = d.toLocaleString(en,US,)
     // console.log(n)
 
     let c = weatherObj.main.temp - 273.15
-    //console.log(c)
     let f = (c * 1.8) + 32
-    //console.log(f)
+    let fTotal = Math.round(f)
+  
     return `
         <section id="weather">
-            <div>${month}/${day}/${year}</div>
-            <div>Fahrenheit Temp: ${f}</div>
+            <div>${d}</div>
+            
+            <div>Fahrenheit Temp: ${fTotal}</div>
             <div>Humidity: ${weatherObj.main.humidity}%</div>
             <div>${weatherObj.weather[0].description}</div>
             <div>" " </div>
