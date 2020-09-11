badaeebbc1a0e3ca12c44f27f091e67b0a0af4a8
Abdurehman05 <abdukr5@gmail.com>
2020-09-11 10:25:57 -0500

    api keys fixed in .setting

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,12 @@
-
 import { EaterySelect } from "./eateries/EaterySelect.js";
 import { AttractionSelect } from "./attractions/AttractionSelect.js";
+import { ParkSelect } from "./parks/ParkSelected.js";
+import { weatherList } from "./weather/weatherList.js";
+import { getWeather } from "./weather/WeatherProvider.js";
+import { testing } from "./itinerarypreview/ItineraryPreviewEatery.js";
 
 EaterySelect();
 AttractionSelect();
-
-
-
-import { ParkSelect } from "./parks/ParkSelected.js"
-
-
-
-
-ParkSelect()
-
-import { EaterySelect } from './eateries/EaterySelect.js'
-import { weatherList } from './weather/weatherList.js';
-import { getWeather } from './weather/WeatherProvider.js';
-
-EaterySelect();
-
-
-
+ParkSelect();
 weatherList();
-
-import { testing } from './itinerarypreview/ItineraryPreviewEatery.js'
-
 testing();
-
-
