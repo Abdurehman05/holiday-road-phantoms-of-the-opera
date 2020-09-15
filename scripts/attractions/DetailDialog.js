b6b4f60abefb9a33987b6b943ce0f7ad725717cb
Abdurehman05 <abdukr5@gmail.com>
2020-09-15 12:16:03 -0500

    detail module modified

--- scripts/attractions/DetailDialog.js
+++ scripts/attractions/DetailDialog.js
@@ -3,29 +3,30 @@ import { useAttractions } from "./AttractionProvider.js";
 const eventHub = document.querySelector(".container");
 
 eventHub.addEventListener("attractionClicked", event => {
-            const detailAttraction = useAttractions().find(attraction => {
-                return attraction.id === parseInt(event.detail.chosenAttraction);
-            });
-            const attractionTarget = document.querySelector(
-                `.attraction--${detailAttraction.id}`
-            );
-            const hTarget = document.querySelector("h4");
+    const detailAttraction = useAttractions().find(attraction => {
+        return attraction.id === parseInt(event.detail.chosenAttraction);
+    });
+    const attractionTarget = document.querySelector(
+        `.attraction--${detailAttraction.id}`
+    );
+    const hTarget = document.querySelector("h4");
 
-            if (attractionTarget.contains(hTarget)) {
-                attractionTarget.innerHTML = "";
-            } else {
-                attractionTarget.innerHTML = `${(detailAttraction.ameneties,
-    attraction => {
-      return `
+    if (attractionTarget.contains(hTarget)) {
+        attractionTarget.innerHTML = "";
+    } else {
+        attractionTarget.innerHTML =
+            (detailAttraction,
+                () => {
+                    return `
             <h4>Souvenirs: ${attraction.souvenirs}</h4>
             <h4>Restrooms: ${attraction.restrooms}</h4>
 				`;
-    })}`;
-  }
+                });
+    }
 });
 
 export const AttractionDialog = id => {
-  return `
+    return `
 		<span class="attraction--${id}"></span>
 	`;
 };
